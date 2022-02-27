0b8187fa0b1d27bddf680260de8c0319e104a82a
Simon Walker <s.r.walker101@googlemail.com>
2022-02-27 23:08:42 +0000

    Test with race detector on

--- internal/lambdahost/host_test.go
+++ internal/lambdahost/host_test.go
@@ -2,6 +2,7 @@ package lambdahost
 
 import (
 	"context"
+	"sync"
 	"testing"
 
 	"github.com/mindriot101/lambda-local-runner/internal/docker"
@@ -11,17 +12,34 @@ type call struct {
 	name string
 }
 
-
 type mockClient struct {
+	mu    sync.Mutex
 	calls []call
 }
 
+func (m *mockClient) NumCalls() int {
+	return len(m.Calls())
+}
+
+func (m *mockClient) Calls() []call {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	return m.calls
+}
+
 func (m *mockClient) RunContainer(ctx context.Context, args docker.RunContainerArgs) (string, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	m.calls = append(m.calls, call{"RunContainer"})
 	return "containerID", nil
 }
 
 func (m *mockClient) RemoveContainer(ctx context.Context, containerID string) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	m.calls = append(m.calls, call{"RemoveContainer"})
 	return nil
 }
@@ -36,17 +54,18 @@ func TestShutdown(t *testing.T) {
 	host.Shutdown()
 	<-done
 
-	nCalls := len(client.calls)
+	calls := client.Calls()
+	nCalls := len(calls)
 	if nCalls != 2 {
 		t.Fatalf("invalid number of calls, found %d expected %d", nCalls, 2)
 	}
 
-	if client.calls[0].name != "RunContainer" {
-		t.Fatalf("invalid call %s expected RunContainer", client.calls[0].name)
+	if calls[0].name != "RunContainer" {
+		t.Fatalf("invalid call %s expected RunContainer", calls[0].name)
 	}
 
-	if client.calls[1].name != "RemoveContainer" {
-		t.Fatalf("invalid call %s expected RemoveContainer", client.calls[1].name)
+	if calls[1].name != "RemoveContainer" {
+		t.Fatalf("invalid call %s expected RemoveContainer", calls[1].name)
 	}
 }
 
@@ -61,24 +80,25 @@ func TestRestart(t *testing.T) {
 	host.Shutdown()
 	<-done
 
-	nCalls := len(client.calls)
+	calls := client.Calls()
+	nCalls := len(calls)
 	if nCalls != 4 {
 		t.Fatalf("invalid number of calls, found %d expected %d", nCalls, 4)
 	}
 
-	if client.calls[0].name != "RunContainer" {
-		t.Fatalf("invalid call %s expected RunContainer", client.calls[0].name)
+	if calls[0].name != "RunContainer" {
+		t.Fatalf("invalid call %s expected RunContainer", calls[0].name)
 	}
 
-	if client.calls[1].name != "RemoveContainer" {
-		t.Fatalf("invalid call %s expected RemoveContainer", client.calls[1].name)
+	if calls[1].name != "RemoveContainer" {
+		t.Fatalf("invalid call %s expected RemoveContainer", calls[1].name)
 	}
 
-	if client.calls[2].name != "RunContainer" {
-		t.Fatalf("invalid call %s expected RunContainer", client.calls[2].name)
+	if calls[2].name != "RunContainer" {
+		t.Fatalf("invalid call %s expected RunContainer", calls[2].name)
 	}
 
-	if client.calls[3].name != "RemoveContainer" {
-		t.Fatalf("invalid call %s expected RemoveContainer", client.calls[3].name)
+	if calls[3].name != "RemoveContainer" {
+		t.Fatalf("invalid call %s expected RemoveContainer", calls[3].name)
 	}
 }
